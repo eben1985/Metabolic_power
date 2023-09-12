fdfb42f1897dde9dd702e7ed38f60c5885fe5e13
eben1985 <eben.oppi@gmail.com>
2023-09-13 09:09:02 +1000

    change of title

--- Metabolic_power.py
+++ Metabolic_power.py
@@ -32,7 +32,7 @@ def data_concat(data_frames):
 
 def main():
     st.set_page_config(layout='wide')
-    st.title('MMetabolic Power Analysis')
+    st.title('Metabolic Power Analysis')
 
     uploaded_files = st.file_uploader("Select all CSV files", type=["csv"], accept_multiple_files=True)
 
